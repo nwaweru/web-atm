54631109a1822d7155ceb95718d89db1b2ae2437
= <ndiranguwaweru@gmail.com>
2018-08-22 17:54:21 +0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@include('layouts.header')
+@include('layouts.navigation')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <br><br><br><br><br><br><br><br><br><br><br>
+            <h1 class="text-center">Web ATM</h1>
+        </div>
+    </div>
+</div>
+
+@include('layouts.footer')
